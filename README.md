356bf11cdab78081767d82d410f520631063a61e
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2021-12-01 16:48:53 -0800

    Added Lab 3. Updated lists to Html. Added functionality to user guesser game

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # about-me-201
+
+## LAB - 02 11/30/21
+
+- Started a new website. Added formatiing in CSS. Added five questions to alert the user when visiting the site.
+
+## LAB - 03 12/1/21
+
+- Main goal was to build of of lab two and add functionality to the 'About Me Guessing Game'. Added three lists to html. Added two more questions to alert the user when visiting the site. Added a tracker that keeps count of the correct answers. The user will be prompter how many they get correct at the end of the alert phase.
--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-//console.log('hello world')
+let totalScore=0;
+let totalQuestions=7;
 
 let user = prompt('What is your name?');
 
@@ -9,15 +10,16 @@ alert('Welcome '+ user + '!');
 
 // Question One
 
-let answerOne = prompt('Would you be an astronaut?');
+let answerOne = prompt('Do I like to climb?');
 
 if (answerOne.toLowerCase() === 'yes' || answerOne.toLowerCase() === 'y'){
   //console.log('Yes I would be an astronaut');
-  alert('Yes I would be an astronaut');
+  totalScore=totalScore+1;
+  alert('Yes I do like to climb!');
 }
-else if (answerOne.toLocaleLowerCase() === 'no' || answerOne.toLowerCase() === 'n'){
-  //console.log('No I would not be an astronaut');
-  alert('No I would not be an astronaut');
+else if (answerOne.toLowerCase() === 'no' || answerOne.toLowerCase() === 'n'){
+  //console.log('Sorry');
+  alert('Sorry, but I do like to climb!');
 }
 else {
   //console.log('That is not a correct response, please reply "yes" or "no"');
@@ -27,15 +29,16 @@ else {
 
 // Question Two
 
-let answerTwo = prompt('Do you faint easily?');
+let answerTwo = prompt('Have I worked as a wildland firefighter?');
 
-if (answerTwo.toLocaleLowerCase() === 'yes' || answerTwo.toLowerCase() === 'y'){
-  //console.log('Yes I do faint easily');
-  alert('Yes I do faint easily');
+if (answerTwo.toLowerCase() === 'yes' || answerTwo.toLowerCase() === 'y'){
+  //console.log('Yes I have worked as a wildland firefighter!');
+  totalScore=totalScore+1;
+  alert('Yes I have worked as a wildland firefighter!');
 }
-else if (answerTwo.toLocaleLowerCase() === 'no' || answerTwo.toLowerCase() === 'n'){
-  //console.log('No I do not faint easily');
-  alert('No I do not faint easily');
+else if (answerTwo.toLowerCase() === 'no' || answerTwo.toLowerCase() === 'n'){
+  //console.log('Sorry, but I have worked as a wildland firefighter!');
+  alert('Sorry, but I have worked as a wildland firefighter!');
 }
 else {
   //console.log('That is not a correct response, please reply "yes" or "no"');
@@ -45,15 +48,16 @@ else {
 
 // Question Three
 
-let answerThree = prompt('Are you afraid of heights?');
+let answerThree = prompt('Am I afraid of heights?');
 
-if (answerThree.toLocaleLowerCase() === 'yes' || answerThree.toLowerCase() === 'y'){
-  //console.log('Yes I am afraid of heights');
-  alert('Yes I am afraid of heights');
+if (answerThree.toLowerCase() === 'yes' || answerThree.toLowerCase() === 'y'){
+  //console.log('Sorry but I am not afraid of heights.');
+  alert('Sorry but I am not afraid of heights.');
 }
-else if (answerThree.toLocaleLowerCase() === 'no' || answerThree.toLowerCase() === 'n'){
-  //console.log('No I am not afraid of heights');
-  alert('No I am not afraid of heights');
+else if (answerThree.toLowerCase() === 'no' || answerThree.toLowerCase() === 'n'){
+  //console.log('Correct! I am not afraid of heights.');
+  totalScore=totalScore+1
+  alert('Correct! I am not afraid of heights.');
 }
 else {
   //console.log('That is not a correct response, please reply "yes" or "no"');
@@ -65,13 +69,14 @@ else {
 
 let answerFour = prompt('Can you speak spanish?');
 
-if (answerFour.toLocaleLowerCase() === 'yes' || answerFour.toLowerCase() === 'y'){
-  //console.log('Yes I can speak spanish');
-  alert('Yes I can speak spanish');
+if (answerFour.toLowerCase() === 'yes' || answerFour.toLowerCase() === 'y'){
+  //console.log('Sorry, but I would not consider myself capable of speaking Spanish!');
+  alert('Sorry, but I would not consider myself capable of speaking Spanish!');
 }
-else if (answerFour.toLocaleLowerCase() === 'no' || answerFour.toLowerCase() === 'n'){
-  //console.log('No I cannot speak Spanish');
-  alert('No I cannot speak Spanish');
+else if (answerFour.toLowerCase() === 'no' || answerFour.toLowerCase() === 'n'){
+  //console.log('Correct! I cannot speak Spanish. Yet...');
+  totalScore=totalScore+1;
+  alert('Correct! I cannot speak Spanish. Yet...');
 }
 else {
   //console.log('That is not a correct response, please reply "yes" or "no"');
@@ -81,15 +86,16 @@ else {
 
 // Question Five
 
-let answerFive = prompt('Have you ever held a monkey?');
+let answerFive = prompt('Have I ever held a monkey?');
 
-if (answerFive.toLocaleLowerCase() === 'yes' || answerFive.toLowerCase() === 'y'){
-  //console.log('Yes I have held a monkey');
-  alert('Yes, my name is '+ user + ', and I am the MONKEY OVERLORD!');
+if (answerFive.toLowerCase() === 'yes' || answerFive.toLowerCase() === 'y'){
+  //console.log('Yes, I have held a monkey!');
+  totalScore=totalScore+1;
+  alert('Yes, I have held a monkey!');
 }
-else if (answerFive.toLocaleLowerCase() === 'no' || answerFive.toLowerCase() === 'n'){
+else if (answerFive.toLowerCase() === 'no' || answerFive.toLowerCase() === 'n'){
   //console.log('No I have not held a monkey');
-  alert('No, my name is '+ user + ', and I am not the monkey overlord...');
+  alert('Sorry, but I have held a monkey!');
 }
 else {
   //console.log('That is not a correct response, please reply "yes" or "no"');
@@ -97,7 +103,7 @@ else {
 }
 
 
-// When the user scrolls the page, execute myFunction
+//When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
 // Get the header
@@ -114,3 +120,51 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+
+let attempts = 4;
+let correctAnswer = '60'
+
+
+for (let i=0; i<attempts; i++){
+  let questionSix= prompt('Guess a number between 1 and 100');
+    if (questionSix === correctAnswer) {
+      alert('YOU HAVE GUESSED THE CORRECT ANSWER');
+      //console.log (questionSix);
+      totalScore=totalScore+1;
+      break;
+    }
+    else if (questionSix > correctAnswer) {
+      alert('You have guessed too high, please try again.')
+      //console.log (questionSix);
+    }
+    else if (questionSix < correctAnswer) {
+      alert('You have guessed to low, please try again.')
+      //console.log (questionSix);
+    }
+}
+
+alert(`The correct answer was ${correctAnswer}`)
+
+let secondAttempts = 6;
+let fellowship = ['gandalf','aragorn','gimli','legolas','frodo','samwise','mary','pippin','boromir'];
+
+console.log(fellowship);
+
+for (let p=0; p<secondAttempts; p++){
+  let questionSeven= prompt('Name a character from the original fellowship of the ring (that left Rivendell). ');
+    if (fellowship.includes(questionSeven.toLowerCase())) {
+      alert('YOU HAVE GUESSED THE CORRECT ANSWER');
+      totalScore=totalScore+1;
+      break;
+    }
+    else if (questionSeven != fellowship) {
+      alert('That is not the right answer please try again.');
+      //console.log(questionSeven);
+    }
+}
+
+alert(`The correct answer could have been any of the following; ${ fellowship}.`)
+alert(`Your total Score was ${totalScore} out of ${totalQuestions}.`)
+
+
